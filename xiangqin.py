beddfcb9e6c6998c29228ed8204b36ad4af1338c
zhjfeng <15850512966@163.com>
2019-03-14 17:47:57 +0800

    完成脚本爬取，写入txt

--- xiangqin.py
+++ xiangqin.py
@@ -4,35 +4,47 @@ author:zhjfeng
 date:20190319
 '''
 import json
-import smtplib
+import re
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://share.zaixs.com/wap/community/list"
-
 #page是页数从1开始，每页40条信息
-querystring = {"fid":"67","ordertype":"1","page":"2"}
+querystring = {"fid":"67","ordertype":"1","page":"1"}
 #网站做了防爬，直接发请求会403，这里要在headers里加上User-Agent
-headers = {
-    'User-Agent': "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Mobile Safari/537.36"
-    }
-#json格式化
-response = requests.request("POST", url, headers=headers, params=querystring).json()
-for i in range(0,39):
+headers = {'User-Agent': "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Mobile Safari/537.36"}
+url1 = "https://share.zaixs.com/wap/thread/view-thread/tid/"
 
-	tid = response["data"]["list"]["thread"][i]["tid"]
-	print(tid)
-#print(response)
-#
+def gettid():
+#访问征婚版块，获取该板块下的帖子id
+	response = requests.request("POST", url, headers=headers, params=querystring).json()
+	#print(response)
+	tidlist = []
+	#每页返回上限为40条，循环输出tid保存到tidlist中
+	for i in range(0,40):		
+		tid = response["data"]["list"]["thread"][i]["tid"]
+		tidlist.append(tid)
+	return tidlist	
+#print(gettid())
 
-
-#链接拼接
-url = "https://share.zaixs.com/wap/thread/view-thread/tid/661269"
-
-headers = {
-    'Cache-Control': "no-cache",
-    'Postman-Token': "8ad297fa-fb0a-48db-a9c6-422ddc5cd73d"
-    }
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
+#链接拼接后访问 
+messagelist = []
+def getmessage():
+	for j in gettid():
+		url2 = url1+ str(j)
+		response = requests.request("GET", url2, headers=headers)
+		#html格式化
+		soup = BeautifulSoup(response.text,'html.parser')
+		#print(soup)
+		#根据标签取出个人信息
+		message = soup.select('.typeoption')[0].text
+		#print(message)
+		messagelist.append(message)
+	return messagelist
+#print(getmessage())
+#返回写入txt
+f = open('E:/git/changtang/message.txt','w')
+for x in getmessage():
+    y = str(x) + '\n'
+    f.write(y)
+f.close()
